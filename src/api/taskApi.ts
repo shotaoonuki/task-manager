d6a6061496bfaa36f423f770a26de2df95e2424a
shotaoonuki <daguansho@gmail.com>
2025-11-14 07:13:26 +0900

    Create taskApi.ts

---
+++ src/api/taskApi.ts
@@ -0,0 +1,21 @@
+import { api } from "./axiosInstance";
+import type { Task } from "../types/task";
+
+export async function getTasks(): Promise<Task[]> {
+  const res = await api.get("/tasks");
+  return res.data;
+}
+
+export async function addTask(task: Partial<Task>): Promise<Task> {
+  const res = await api.post("/tasks", task);
+  return res.data;
+}
+
+export async function deleteTask(id: number): Promise<void> {
+  await api.delete(`/tasks/${id}`);
+}
+
+export async function updateTask(id: number, task: Partial<Task>): Promise<Task> {
+  const res = await api.put(`/tasks/${id}`, task);
+  return res.data;
+}
