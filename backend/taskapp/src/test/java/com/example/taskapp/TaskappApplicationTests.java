2d798f8221622c2165cc8d1fccd029f20c683e98
shotaoonuki <daguansho@gmail.com>
2025-11-16 14:04:36 +0900

    Update TaskappApplicationTests.java

--- backend/taskapp/src/test/java/com/example/taskapp/TaskappApplicationTests.java
+++ backend/taskapp/src/test/java/com/example/taskapp/TaskappApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.taskapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TaskappApplicationTests {
 
 	@Test
