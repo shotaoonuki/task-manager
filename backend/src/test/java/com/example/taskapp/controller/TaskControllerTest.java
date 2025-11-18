9bb03d1e2345abd514b269724d35b9ff383951d5
shotaoonuki <daguansho@gmail.com>
2025-11-18 10:28:39 +0900

    Update TaskControllerTest.java

--- backend/src/test/java/com/example/taskapp/controller/TaskControllerTest.java
+++ backend/src/test/java/com/example/taskapp/controller/TaskControllerTest.java
@@ -14,7 +14,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(classes = TaskappApplication.class)
-@AutoConfigureMockMvc
+@AutoConfigureMockMvc(addFilters = false) 
 @ActiveProfiles("test")
 class TaskControllerTest {
 
