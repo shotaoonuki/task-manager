c14ccaf427ef6ee02fdc625ac1ed30a010044ecd
shotaoonuki <128361885+shotaoonuki@users.noreply.github.com>
2025-12-20 07:20:31 +0900

    Update TaskControllerTest.java

--- backend/src/test/java/com/example/taskapp/controller/TaskControllerTest.java
+++ backend/src/test/java/com/example/taskapp/controller/TaskControllerTest.java
@@ -12,7 +12,6 @@ import org.springframework.test.context.jdbc.Sql;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
